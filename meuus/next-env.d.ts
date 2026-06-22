c4420b235873d801fc10a31230e1cbb292d7690b
Md. Mahady Hasan <md.mahadyhasan.ruju@gmail.com>
2026-06-22 17:12:58 +0600

    chore: remove accidental Next.js generated artifacts (#7)

--- meuus/next-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-import "./.next/types/routes.d.ts";
-import "./.next/types/cache-life.d.ts";
-import "./.next/types/validator.ts";
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
